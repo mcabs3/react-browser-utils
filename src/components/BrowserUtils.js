784708af18399f8fe0110dec0b977c8cbb33c79b
Miguel Caballero <mcabs3@gmail.com>
2017-12-14 15:53:20 -0600

    fixed missing method

--- src/components/BrowserUtils.js
+++ src/components/BrowserUtils.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { saveDimensionData } from '../redux/Reducer';
+import { getBrowserData } from '../utils/helpers';
 
 
 const mapState = () => ({});
